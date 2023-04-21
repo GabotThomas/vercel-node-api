7e231cf00190ecf9a1468f01c956046a229c1ee7
GabotThomas <gabotthomas@gmail.com>
2023-04-21 13:30:01 +0200

    update

--- supply.js
+++ supply.js
@@ -13,21 +13,57 @@ const router = express.Router();
 
 // Handling request using router
 router.post('/', (req, res, next) => {
+	console.log('body :');
 	const body = req.body;
+
 	if (!body || !body.products) return res.status(404).send();
-	Promise.all(
-		body.products.map((product, index) => {
-			const body = {
-				quantity: product.quantity,
-				productId: index,
-				status: 'supply',
-			};
-			return fetching(`http://127.0.0.1:5000/api/stock/${index}/movement`, 'POST', body);
-		})
-	);
+	try {
+		const responses = Promise.all(
+			body.products.map((product, index) => {
+				if (product.id) {
+					return addStock(product);
+				} else {
+					return createProduct(product);
+				}
+			})
+		);
+	} catch (e) {
+		console.log('test');
+		console.log(e);
+	}
 
 	res.status(204).send();
 });
+
+const addStock = async product => {
+	const body = {
+		quantity: product.quantity,
+		productId: product.id,
+		status: 'supply',
+	};
+	return fetching(`http://127.0.0.1:5000/api/stock/${product.id}/movement`, 'POST', body);
+};
+
+const createProduct = async product => {
+	const body = {
+		ean: product.ean,
+		name: product.name,
+		description: product.description,
+		categories: ['game'],
+		price: product.purchasePricePerUnit,
+	};
+	const response = await fetching(
+		`http://microservices.tp.rjqu8633.odns.fr/api/products`,
+		'POST',
+		body
+	);
+
+	if (response.id) {
+		return addStock({ ...product, id: response.id });
+	}
+
+	return;
+};
 
 // Importing the router
 module.exports = router;
